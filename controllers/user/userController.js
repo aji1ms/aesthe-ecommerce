ffd519eff02b916a850ff8814ff68f18db377d81
Ajims Ismail <ajimsismail123@gmail.com>
2025-02-12 17:23:58 +0530

    project initialized

---
+++ controllers/user/userController.js
@@ -0,0 +1,24 @@
+
+
+const pageNotFound = async (req, res) => {
+    try {
+        res.render('page-404')
+    } catch (error) {
+        res.redirect('/pageNotFound')
+    }
+}
+
+
+const loadHomepage = async (req, res) => {
+    try {
+        res.render("home");
+    } catch (error) {
+        console.log("Home Page Not Found")
+        res.status(500).send("Server Error")
+    }
+}
+
+module.exports = {
+    loadHomepage,
+    pageNotFound,
+} 
