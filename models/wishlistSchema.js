d394c2c0b89f9d730139897b7cf039c755c58c13
Ajims Ismail <ajimsismail123@gmail.com>
2025-02-13 12:44:58 +0530

    Schema created

---
+++ models/wishlistSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const wishlistSchema = new mongoose.Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    products: [{
+        productId: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+            required: true
+        },
+        addedOn: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+})
+
+const Wishlist = mongoose.model("Wishlist", wishlistSchema);
+module.exports = Wishlist;
