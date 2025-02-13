32b851565952def1b99251447f62052bdd34da2e
Ajims Ismail <ajimsismail123@gmail.com>
2025-02-13 23:59:25 +0530

    created login and signup pages

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,24 +1,64 @@
-
-
-const pageNotFound = async (req, res) => {
-    try {
-        res.render('page-404')
-    } catch (error) {
-        res.redirect('/pageNotFound')
-    }
-}
+// ---Home page---
 
 
 const loadHomepage = async (req, res) => {
     try {
         res.render("home");
     } catch (error) {
-        console.log("Home Page Not Found")
+        console.log("Home Page Not Found", error)
         res.status(500).send("Server Error")
     }
 }
 
+// ---Signup page---
+
+const loadSignup = async (req, res) => {
+    try {
+        res.render("signup");
+    } catch (error) {
+        console.log("Sign Up page Loading Error", error)
+        res.render('/pageNotFound')
+    }
+}
+
+// ---Register page---
+
+const loadRegisterpage = async (req, res) => {
+    try {
+        res.render("register")
+    } catch (error) {
+        console.log("Register Page Loading Error", error);
+        res.render('/pageNotFound');
+    }
+}
+
+// ---Register page---
+
+const loadloginpage = async (req, res) => {
+    try {
+        res.render("login")
+    } catch (error) {
+        console.log("Login Page Loading Error", error);
+        res.render('/pageNotFound')
+    }
+}
+
+// ---404 page---
+
+const pageNotFound = async (req, res) => {
+    try {
+        res.render('page-404')
+    } catch (error) {
+        console.log("Page-404 Loading Error", error)
+        res.redirect('/pageNotFound')
+    }
+}
+
+
 module.exports = {
     loadHomepage,
     pageNotFound,
+    loadSignup,
+    loadRegisterpage,
+    loadloginpage,
 } 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/user/userController");
 
-router.get('/pageNotFound',userController.pageNotFound);
 
-router.get('/',userController.loadHomepage);
+router.get('/', userController.loadHomepage);
+router.get('/signup', userController.loadSignup);
+router.get('/register', userController.loadRegisterpage);
+router.get('/login',userController.loadloginpage);
+router.get('/pageNotFound', userController.pageNotFound);
 
 module.exports = router;
