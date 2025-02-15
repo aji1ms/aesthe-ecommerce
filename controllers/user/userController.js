efc136b63365720d451086b21392a97f592b4f43
Ajims Ismail <ajimsismail123@gmail.com>
2025-02-15 20:32:22 +0530

    otp verification and resend otp implemented

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,18 +1,18 @@
 
-const User = require("../../models/userSchema")
+const { Session } = require("express-session");
+const User = require("../../models/userSchema");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+const env = require("dotenv").config();
 
 // ---Home page---
 
 const loadHomepage = async (req, res) => {
     try {
-
         res.render("home");
-
     } catch (error) {
-
         console.log("Home Page Not Found", error)
         res.status(500).redirect("/pageNotFound")
-
     }
 }
 
@@ -20,14 +20,10 @@ const loadHomepage = async (req, res) => {
 
 const loadSignup = async (req, res) => {
     try {
-
         res.render("signup");
-
     } catch (error) {
-
         console.log("Sign Up page Loading Error", error)
         res.status(500).redirect("/pageNotFound")
-
     }
 }
 
@@ -35,33 +31,159 @@ const loadSignup = async (req, res) => {
 
 const loadRegisterpage = async (req, res) => {
     try {
-
         res.render("register")
-
     } catch (error) {
-
         console.log("Register Page Loading Error", error);
         res.status(500).redirect('/pageNotFound');
 
     }
 }
 
-const register = async (req,res) => {
+// ---Register otp generation---
 
-    const {name,email,phone,password} = req.body;
+function generateOtp() {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+async function sendVerificationEmail(email, otp) {
+    try {
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            port: 587,
+            secure: false,
+            requireTLS: true,
+            auth: {
+                user: process.env.NODEMAILER_EMAIL,
+                pass: process.env.NODEMAILER_PASSWORD,
+            }
+        })
+
+        const info = await transporter.sendMail({
+            from: process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: "Verify your account",
+            text: `Your OTP is ${otp}`,
+            html: `<b>Your OTP: ${otp}</b>`,
+        })
+
+        return info.accepted.length > 0;
+
+    } catch (error) {
+        console.error("Error sending email,", error);
+        return false;
+    }
+}
+
+// ---user registration---
+
+const register = async (req, res) => {
+
+    const { name, email, phone, password } = req.body;
 
     try {
 
-        const newUser = new User({name,email,phone,password});
-        console.log(newUser);
-        await newUser.save();
-        res.redirect('/login')
+        const { email, password, cPassword } = req.body;
+        if (password != cPassword) {
+            return res.render("register", { message: "Password do not match" });
+        }
+
+        const findUser = await User.findOne({ email });
+        if (findUser) {
+            return res.render("register", { message: "Email already exists" })
+        }
+
+        const otp = generateOtp();
+        const emailSent = await sendVerificationEmail(email, otp);
+        if (!emailSent) {
+            return res.json("email-error")
+        }
+
+        req.session.otp = otp;
+        req.session.userData = { name, email, phone, password };
+
+        res.render("verify-otp");
+        console.log("OTP Sent :", otp);
 
     } catch (error) {
+        console.log("Register Error", error);
+        res.redirect("/pageNotFound");
+    }
+}
 
-        console.log("Error for save user",error);
-        res.status(500).redirect("/pageNotFound")
 
+const verifyOtp = async (req, res) => {
+    try {
+        const { otp } = req.body;
+        const storedOTP = req.session.otp;
+        const userData = req.session.userData;
+
+        console.log("Received OTP from user:", otp);
+        console.log("Stored OTP in session:", req.session.otp);
+        console.log("Stored User Data:", req.session.userData);
+
+
+        if (otp !== storedOTP) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Invalid OTP. Please try again.'
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+        const saveUserData = new User({
+            name: userData.name,
+            phone: userData.phone,
+            email: userData.email,
+            password: hashedPassword,
+            isVerified: true
+        });
+
+        await saveUserData.save();
+
+        // Clear session data
+        req.session.otp = null;
+        req.session.userData = null;
+        req.session.otpTimestamp = null;
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'Account verified successfully!',
+            redirectUrl: '/'
+        });
+
+    } catch (error) {
+        console.error("OTP verification error:", error);
+        return res.status(500).json({
+            status: 'error',
+            message: 'Failed to verify OTP.'
+        });
+    }
+};
+
+// ---resend OTP ---
+
+const resendOtp = async (req, res) => {
+    try {
+        const { email } = req.session.userData;
+        if (!email) {
+            res.status(400).json({ success: false, message: "Email not found in session" });
+        }
+        const otp = generateOtp();
+        req.session.otp = otp;
+
+        const emailSent = await sendVerificationEmail(email, otp);
+        if (emailSent) {
+            console.log("Resend OTP : ", otp);
+            res.status(200).json({ success: true, message: "OTP Resend Successfully" });
+        } else {
+            res.status(500).json({ success: false, message: "Failed to resend OTP. Please try again" });
+        }
+    } catch (error) {
+        console.error("Error resending OTP", error);
+        res.status(500).json({ success: false, message: "Internal server Error. please try again" });
     }
 }
 
@@ -69,14 +191,10 @@ const register = async (req,res) => {
 
 const loadloginpage = async (req, res) => {
     try {
-
         res.render("login")
-
     } catch (error) {
-
         console.log("Login Page Loading Error", error);
         res.status(500).redirect("/pageNotFound")
-
     }
 }
 
@@ -84,14 +202,10 @@ const loadloginpage = async (req, res) => {
 
 const pageNotFound = async (req, res) => {
     try {
-
         res.render('page-404')
-
     } catch (error) {
-
         console.log("Page-404 Loading Error", error)
         res.status(500).redirect("/pageNotFound")
-
     }
 }
 
@@ -102,5 +216,7 @@ module.exports = {
     loadSignup,
     loadRegisterpage,
     register,
+    verifyOtp,
+    resendOtp,
     loadloginpage,
 } 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,6 +9,9 @@ router.get('/signup', userController.loadSignup);
 router.get('/register', userController.loadRegisterpage);
 router.post('/register', userController.register);
 
+router.post('/verify-otp',userController.verifyOtp);
+router.post('/resend-otp',userController.resendOtp);
+
 router.get('/login',userController.loadloginpage);
 router.get('/pageNotFound', userController.pageNotFound);
 
