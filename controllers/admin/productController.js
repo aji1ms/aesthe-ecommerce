ae74e874cefa038751a27db181a32a01104b50bd
Ajims Ismail <ajimsismail123@gmail.com>
2025-02-26 18:03:21 +0530

    Products block/unblock implemented

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -116,10 +116,34 @@ const getAllProducts = async (req, res) => {
             })
         } else {
             res.render("admin-error")
-        }
+        } 
 
     } catch (error) {
         res.redirect("errorpage");
+    }
+}
+
+
+const blockProduct = async (req, res) => {
+    try {
+
+        let id = req.query.id; 
+        await Product.updateOne({ _id: id }, { $set: { isBlocked: true } });
+        res.redirect("/admin/products");
+
+    } catch (error) {
+        res.redirect("/errorpage");
+    }
+}
+ 
+const unblockProduct = async (req, res) => {
+    try {
+        let id = req.query.id;
+        await Product.updateOne({ _id: id }, { $set: { isBlocked: false } });
+        res.redirect("/admin/products");
+
+    } catch (error) {
+        res.redirect("/errorpage");
     }
 }
 
@@ -127,4 +151,6 @@ module.exports = {
     getProductAddPage,
     addProducts,
     getAllProducts,
+    blockProduct,
+    unblockProduct,
 }
