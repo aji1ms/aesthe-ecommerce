8d6161cb20b9859c0d221b6e32288e53854e111c
Ajims Ismail <ajimsismail123@gmail.com>
2025-04-08 19:49:35 +0530

    Wallet controller edited

--- controllers/user/walletController.js
+++ controllers/user/walletController.js
@@ -22,7 +22,7 @@ const walletHistory = async (req, res) => {
      
       const userId = req.session.user;
   
-      const transactions = await Transaction.findOne({ user: userId })
+      const transactions = await Transaction.find({ user: req.session.user })
         .populate("user", "name email")
         .sort({ date: -1 });
   
