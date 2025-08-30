6ef0eed2c17da0c6895668050b8417a7be3425fb
Ajims Ismail <ajmalismail678@gmail.com>
2025-08-30 20:10:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
 app.set("view engine", "ejs");
 app.set("views", [path.join(__dirname, "views/user"), path.join(__dirname, "views/admin")]);
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
  
 app.use('/', userRouter);
 app.use('/admin', adminRouter); 
